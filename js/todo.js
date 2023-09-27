526d22344676df03b40e5d9273646f799d3b603e
hymsoft <hymsoft@gmail.com>
2023-09-27 10:01:20 -0300

    Agregado SweetAlert2 para los mensajes

--- js/todo.js
+++ js/todo.js
@@ -7,22 +7,45 @@ document.addEventListener('DOMContentLoaded', function () {
     btnAgregar.addEventListener('click', function (event) {
         event.preventDefault();
         const tareaTexto = inputTarea.value;
+        if (tareaTexto.trim() === '') {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'No se pueden agregar tareas vacias...',
+                showConfirmButton: false,
+                timer: 2000
+            })
+            return
+        }
 
-        if (tareaTexto.trim() !== '') {
-            const nuevaTarea = document.createElement('li');
-            nuevaTarea.innerHTML = `
+        const nuevaTarea = document.createElement('li');
+        nuevaTarea.innerHTML = `
           <p>${tareaTexto}</p>
           <button class="btn-delete"><i class="bi bi-trash"></i></button>
         `;
-            ulTareas.appendChild(nuevaTarea);
-            emptyMessage.style.display = 'none';
-            inputTarea.value = '';
-        }
+        ulTareas.appendChild(nuevaTarea);
+        emptyMessage.style.display = 'none';
+        inputTarea.value = '';
+
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Tarea agregada!!!',
+            showConfirmButton: false,
+            timer: 1500
+        })
     });
 
     document.addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-delete') || event.target.closest('.btn-delete')) {
             event.target.closest('li').remove(); // Selecciona el elemento li y lo elimina
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tarea eliminada',
+                showConfirmButton: false,
+                timer: 1500
+            })
             if (ulTareas.children.length === 0) {
                 emptyMessage.style.display = 'block';
             }
