28bac63b0f85f299694dabc78ad9079fa217f074
hymsoft <hymsoft@gmail.com>
2023-09-27 11:12:34 -0300

    Agregada persistencia mediante local storage

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,91 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnAgregar = document.querySelector('.btn-add');
     const ulTareas = document.querySelector('ul');
     const emptyMessage = document.querySelector('.empty');
+    cargarTareas()
+
+    function cargarTareas() {
+        try {
+            const tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || [];
+            tareasGuardadas.forEach(function (tareaTexto) {
+                const nuevaTarea = document.createElement('li');
+                nuevaTarea.innerHTML = `
+                <p>${tareaTexto}</p>
+                <button class="btn-delete"></button>
+                `;
+                ulTareas.appendChild(nuevaTarea);
+            });
+
+            if (tareasGuardadas.length > 0) {
+                emptyMessage.style.display = 'none';
+            }
+        } catch (error) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Tarea agregada!!!',
+                text: error,
+                showConfirmButton: true,
+                timer: 3000
+            })
+        }
+    }
+
+    function guardarTarea(tareaTexto) {
+        try {
+            const tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || [];
+            tareasGuardadas.push(tareaTexto);
+            localStorage.setItem('tareas', JSON.stringify(tareasGuardadas));
+
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tarea agregada!!!',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } catch (error) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Tarea agregada!!!',
+                text: error,
+                showConfirmButton: true,
+                timer: 3000
+            })
+        }
+    }
+
+    function eliminarTarea(tareaTexto) {
+        try {
+            const tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || [];
+            const index = tareasGuardadas.indexOf(tareaTexto);
+            if (index > -1) {
+                tareasGuardadas.splice(index, 1);
+                localStorage.setItem('tareas', JSON.stringify(tareasGuardadas));
+            }
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tarea eliminada',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            if (ulTareas.children.length === 0) {
+                emptyMessage.style.display = 'block';
+            }
+
+        } catch (error) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Tarea agregada!!!',
+                text: error,
+                showConfirmButton: true,
+                timer: 3000
+            })
+        }
+    }
+
 
     btnAgregar.addEventListener('click', function (event) {
         event.preventDefault();
@@ -21,34 +106,19 @@ document.addEventListener('DOMContentLoaded', function () {
         const nuevaTarea = document.createElement('li');
         nuevaTarea.innerHTML = `
           <p>${tareaTexto}</p>
-          <button class="btn-delete"><i class="bi bi-trash"></i></button>
+          <button class="btn-delete"></button>
         `;
         ulTareas.appendChild(nuevaTarea);
         emptyMessage.style.display = 'none';
         inputTarea.value = '';
-
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Tarea agregada!!!',
-            showConfirmButton: false,
-            timer: 1500
-        })
+        guardarTarea(tareaTexto)
     });
 
     document.addEventListener('click', function (event) {
-        if (event.target.classList.contains('btn-delete') || event.target.closest('.btn-delete')) {
+        if (event.target.classList.contains('btn-delete')) {
+            const tareaTexto = event.target.parentElement.querySelector('p').textContent;
             event.target.closest('li').remove(); // Selecciona el elemento li y lo elimina
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'Tarea eliminada',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            if (ulTareas.children.length === 0) {
-                emptyMessage.style.display = 'block';
-            }
+            eliminarTarea(tareaTexto)
         }
     });
 
